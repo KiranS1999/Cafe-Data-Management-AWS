02352084b2bc40e75aa66e206955da9b6a810642
Kiran Sahiba <kiransahiba9@gmail.com>
2023-01-27 20:35:43 +0000

    removed function from file

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -3,29 +3,13 @@ from extract import extract_clean_data
 import pandas as pd
 
 
-
-
-
-def extract_clean_data(file_to_clean):
-    """take csv file and remove sensitive data
-
-    Args:
-        file_to_clean (string): path to file that would be cleaned
-
-    Returns:
-        Dataframe: file with senstitve information removed
-    """
-    dataframe = pd.read_csv(file_to_clean, header=None)
-    dataframe.drop(dataframe.columns[[2,6]], axis=1, inplace=True)
-    return dataframe
-
 @patch("extract.pd.read_csv")
 
 def test_extract_clean_data(mock_read_csv):
     
     mock_read_csv.return_value = pd.DataFrame(
             {
-                "Date-": "2023-01-01 09:00:00",
+                "Date": "2023-01-01 09:00:00",
                 "location": "Chesterfield",
                 "customer_name": "David Tennant",
                 "items": "Large,Hot chocolate,5.4,Large,Milk latte,2.6" ,
@@ -36,7 +20,7 @@ def test_extract_clean_data(mock_read_csv):
 
     expected = pd.DataFrame(
             {
-                "Date-": "2023-01-01 09:00:00",
+                "Date": "2023-01-01 09:00:00",
                 "location": "Chesterfield",
                 "items": "Large,Hot chocolate,5.4,Large,Milk latte,2.6",
                 "total_amount": "8.00",
